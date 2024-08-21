1454afe9a01a4d82e247a02e653ddf7fc09fdc53
kishimoto <kishimoto88@sanken.osaka-u.ac.jp>
2024-08-22 01:46:22 +0900

    fix

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# from fastapi import FastAPI
-# from csv_upload import router as csv_router  # 追加
-
-# app = FastAPI()
-
-# app.include_router(csv_router)  # 追加
-
 from fastapi import FastAPI
+from csv_upload import router as csv_router  # 追加
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"Hello": "World"}
+app.include_router(csv_router)  # 追加
+
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# def index():
+#     return {"Hello": "World"}
